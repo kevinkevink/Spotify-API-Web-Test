fc130fa7d520ad144a09050227d543aa42134441
khutchins2 <khutchins2@users.noreply.github.com>
2021-09-23 18:52:56 -0500

    accidentally. duplicated file

--- phue.js
+++ phue.js
@@ -1,12 +1,27 @@
-const ACCESS_TOKEN = 
+const AUTHORIZE = "https://api.meethue.com/v2/oauth2/token";
+const DEVICE_ID = "";
+const ADD_NAME = "remotecontrol";
+const DEVICE_ID = "";
+
+var access_token = "";
+var client_id = "nP0PEnKQ7AqmznIy24o6HfbUK1zn0CGu";
+var client_secret = "iACzvnGJ2BhaAACU";
 
 function requestHueAuthorization() {
     let body = {};
     body.linkbutton=true;
 
-    callHueApi("PUT", "https://api.meethue.com/bridge/0/config", body, handleHueAuthorizationResponse());
 }
 
+function HueAuthorization(){
+  let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(REDIRECTURI);
+    url += "&deviceid=" + DEVICE_ID;
+    url += "&devicename=" + APP_NAME;
+    window.location.href = url;
+}
 
 function callHueApi(method, url, body, callback){
     let xhr = new XMLHttpRequest();
